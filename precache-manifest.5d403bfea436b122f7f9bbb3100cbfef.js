d55179eaaea83eb5f4c2b8c4f0ecf26d2ad58494
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-09-15 01:27:33 -0700

    Updates

---
+++ precache-manifest.5d403bfea436b122f7f9bbb3100cbfef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948e5efc878c56c7e7a6efe27ef4d143",
+    "url": "/React-and-Tensorflow/index.html"
+  },
+  {
+    "revision": "d98f6cb60f87d8c3da3f",
+    "url": "/React-and-Tensorflow/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7bfc18da96120b0646fb",
+    "url": "/React-and-Tensorflow/static/css/main.3605943b.chunk.css"
+  },
+  {
+    "revision": "d98f6cb60f87d8c3da3f",
+    "url": "/React-and-Tensorflow/static/js/2.534133ed.chunk.js"
+  },
+  {
+    "revision": "921adb173c5d83dcf71d6a9bc3cbe81a",
+    "url": "/React-and-Tensorflow/static/js/2.534133ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfc18da96120b0646fb",
+    "url": "/React-and-Tensorflow/static/js/main.1d0d1d16.chunk.js"
+  },
+  {
+    "revision": "3a7473759cbba86d60c4",
+    "url": "/React-and-Tensorflow/static/js/runtime-main.5791c5e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-and-Tensorflow/static/media/logo.5d5d9eef.svg"
+  }
+]);
